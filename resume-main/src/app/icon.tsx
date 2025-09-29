92407335a9f27330ebfe6663476ef6882d713212
Kathan1010 <kathangajera@gmail.com>
2025-09-29 11:40:55 +0530

    "Icon edited"

---
+++ resume-main/src/app/icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 20,
+          background: 'hsl(275 100% 25%)',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '8px',
+          fontFamily: '"Playfair Display"',
+          fontWeight: 'bold',
+        }}
+      >
+        KG
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
